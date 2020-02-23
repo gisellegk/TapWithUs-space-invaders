ed830e8206165a9c9ed50c44e758c16a1832d9a9
Giselle Koo <gisellegk@gmail.com>
2020-02-23 10:39:20 -0700

    working on .ino file

--- arduino_src/hackcu_game/hackcu_game.ino
+++ arduino_src/hackcu_game/hackcu_game.ino
@@ -1,18 +1,7 @@
-#include "game_state_machine.h"
+#include "libraries/game_state_machine/game_state_machine.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
-
-#define LED_PIN_1
-#define LED_PIN_2
-#define LED_PIN_3
-#define LED_PIN_4
-#define LED_PIN_5
-#define LED_PIN_6
-#define LED_PIN_7
-
-#define LED_COUNT     7
-#define STRIP_COUNT   8
 
 
 uint32_t currentMillis = 0;
@@ -28,8 +17,8 @@ position bullet;
 
 uint8_t enemy_health = 14;
 uint8_t enemy_touch = 0;
-uint8_t enemy_position = 
-uint8_t player_action_variable ;
+uint8_t enemy_position;
+uint8_t player_action_variable;
 progress_t enemy_progress_by_one = 0;
 
 #define USART_BAUDRATE 9600
@@ -49,17 +38,21 @@ void loop() {
 void SerialEvent() {
   if(Serial.available()){
     char inChar = (char) Serial.read();
+    String inputString = "";
+    inputString += inChar;
     // switch between bunch of events based 
     // what character is received
     if(inputString.equals("3") || inputString.equals("2")){
       //UP
-      
+      if(status.state == sStart) change_event(eStart); //status.event = eStart;
+      else change_event(ePlayerActionShoot); //status.event = ePlayerActionShoot;
     } else if(inputString.equals("6") || inputString.equals("7")){
       //LEFT
+      change_event(ePlayerActionLeft); //status.event = ePlayerActionLeft;
       
     } else if(inputString.equals("8") || inputString.equals("9")){
       //RIGHT
-      
+      change_event(ePlayerActionRight); // status.event = ePlayerActionRight;      
     }
   }
 }
